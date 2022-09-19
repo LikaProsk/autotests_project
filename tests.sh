c644fb871221481641a8d8e62b536b19031a3477
liproskurina <liproskurina@ozon.ru>
2022-09-19 23:01:40 +0300

    add tests

--- tests.sh
+++ tests.sh
@@ -6,6 +6,6 @@ docker run --name tests_run --network $network tests pytest --browser $browser -
 
 docker cp tests_run:/app/allure-report .
 
-allure generate allure-report
+allure generate allure_results
 
 docker rm tests_run
